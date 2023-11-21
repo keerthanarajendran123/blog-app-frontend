7e278fe998f08b3f452c9aaaccef0b33b4cd4c58
keerthanarajendran123 <keerthurb9@gmail.com>
2023-11-21 19:12:24 +0530

    Axios

--- src/utils/ApiService.jsx
+++ src/utils/ApiService.jsx
@@ -1,18 +1,58 @@
-import axios from 'axios'
+// import axios from 'axios'
+
+// const AxiosService = axios.create({
+//     baseURL:`${import.meta.env.VITE_API_URL}`,
+//     headers:{
+//         'Content-Type':"application/json",
+//     }
+// })
+
+// AxiosService.interceptors.request.use(config=>{
+//     //console.log(config)
+//     const token = sessionStorage.getItem('token')
+//     if(token)
+//         config.headers.Authorization = `Bearer ${token}`
+//     return config
+// })
+
+// export default AxiosService  
+
+
+
+import axios from 'axios';
 
 const AxiosService = axios.create({
-    baseURL:`${import.meta.env.VITE_API_URL}`,
-    headers:{
-        'Content-Type':"application/json",
+    baseURL: `${import.meta.env.VITE_API_URL}`,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+AxiosService.interceptors.request.use(
+    (config) => {
+        const token = sessionStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-})
+);
 
-AxiosService.interceptors.request.use(config=>{
-    //console.log(config)
-    const token = sessionStorage.getItem('token')
-    if(token)
-        config.headers.Authorization = `Bearer ${token}`
-    return config
-})
+// Add response interceptor to capture backend errors
+AxiosService.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        // Log or handle the error here
+        console.error('Backend error:', error);
 
-export default AxiosService
+        // Propagate the error further for handling in the components
+        return Promise.reject(error);
+    }
+);
+
+export default AxiosService;
