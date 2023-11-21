e6508e7354fb59a3855ad6c16236854df49ac7c0
keerthanarajendran123 <keerthurb9@gmail.com>
2023-11-21 19:34:20 +0530

    AxiosService updated

--- src/utils/ApiService.jsx
+++ src/utils/ApiService.jsx
@@ -1,58 +1,21 @@
-// import axios from 'axios'
-
-// const AxiosService = axios.create({
-//     baseURL:`${import.meta.env.VITE_API_URL}`,
-//     headers:{
-//         'Content-Type':"application/json",
-//     }
-// })
-
-// AxiosService.interceptors.request.use(config=>{
-//     //console.log(config)
-//     const token = sessionStorage.getItem('token')
-//     if(token)
-//         config.headers.Authorization = `Bearer ${token}`
-//     return config
-// })
-
-// export default AxiosService  
-
-
-
-import axios from 'axios';
+import axios from 'axios'
 
 const AxiosService = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}`,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
-
-AxiosService.interceptors.request.use(
-    (config) => {
-        const token = sessionStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+    baseURL:`${import.meta.env.VITE_API_URL}`,
+    headers:{
+        'Content-Type':"application/json",
     }
-);
+})
 
-// Add response interceptor to capture backend errors
-AxiosService.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        // Log or handle the error here
-        console.error('Backend error:', error);
+AxiosService.interceptors.request.use(config=>{
+    //console.log(config)
+    const token = sessionStorage.getItem('token')
+    if(token)
+        config.headers.Authorization = `Bearer ${token}`
+    return config
+})
 
-        // Propagate the error further for handling in the components
-        return Promise.reject(error);
-    }
-);
+export default AxiosService  
 
-export default AxiosService;
+
+
